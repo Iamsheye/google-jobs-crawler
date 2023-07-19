566bc3be7092a65e051fb122a538bdcac2ae167f
Iamsheye <seyepelumi@gmail.com>
2023-07-19 12:29:46 +0100

    project init

---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'log', 'debug', 'verbose'],
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Google Jobs Scraper')
+    .setDescription('Scrape Google Jobs')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+
+  await app.listen(3333);
+}
+bootstrap();
