c6a286a194ccb408a098d84b3f356eb0c8878d1b
Iamsheye <seyepelumi@gmail.com>
2024-10-18 16:59:56 +0100

    feat(auth): update token expiration to 3 days

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -244,7 +244,7 @@ export class AuthService {
       data: {
         userId,
         token: refreshToken,
-        expiresAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // 7 days from now
+        expiresAt: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000), // 3 days from now
       },
     });
 
